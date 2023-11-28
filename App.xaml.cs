e39095c8f8416ff44011e19696e8616de18a06f1
ANA VÎLCU <ana.vilcu@stud.ubbcluj.ro>
2023-11-28 19:47:19 +0200

    Medii Laborator 8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,26 @@
-﻿namespace Vilcu_Ana_Lab7;
+﻿using System;
+using Vilcu_Ana_Lab7.Data;
+using System.IO;
+
+namespace Vilcu_Ana_Lab7;
 
 public partial class App : Application
 {
+	static ShoppingListDatabase database;
+
+	public static ShoppingListDatabase Database
+	{
+		get
+		{
+			if (database == null)
+			{
+				database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment
+					.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+			}
+			return database;
+		}
+	}
+
 	public App()
 	{
 		InitializeComponent();
